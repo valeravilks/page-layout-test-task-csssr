cf3efb35dd0e6cd2829cec956aba25f3d6e1b42e
Валера <valeravilks@gmail.com>
2019-07-17 19:23:55 +0300

    Perfect textarea

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import './scss/style.scss';
+import $ from 'jquery';
+
+
 
 window.onload = function(){
 
-}
+    $('textarea').each(function () {
+        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+    }).on('input', function () {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+
+
+};
